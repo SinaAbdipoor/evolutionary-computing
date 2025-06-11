433e4602f92b7d042b729bb37f966300168d8e3e
Sina <62741992+SinaAbdipoor@users.noreply.github.com>
2025-06-11 13:56:13 +0800

    Update Main.java

--- OneMax/Main.java
+++ OneMax/Main.java
@@ -1,5 +1,13 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+//        RandomSearch randomSearch = new RandomSearch(5, 10);
+//        RandomSearch randomSearch = new RandomSearch(5, 100);
+//        RandomSearch randomSearch = new RandomSearch(50, 10000);
+//        RandomSearch randomSearch = new RandomSearch(50, 100000);
+        RandomSearch randomSearch = new RandomSearch(50, 1000000);
+//        RandomSearch randomSearch = new RandomSearch(100, 1000000);
+//        RandomSearch randomSearch = new RandomSearch(100, 10000000);
+        randomSearch.run();
     }
 }
