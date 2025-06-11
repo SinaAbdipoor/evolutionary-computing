bc0e4d01b7416df86bc738e6423f86a8e39927c4
Sina <62741992+SinaAbdipoor@users.noreply.github.com>
2025-06-11 13:56:08 +0800

    Create RandomSearch.java

---
+++ OneMax/RandomSearch.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class RandomSearch {
+    private final int problemSize; //The size of the problem, which is the length of the boolean array.
+    private final int maxIterations; //The maximum allowed number of iterations, which, in this case, is actually the same as the Number of Fitness Evaluations (NFE).
+
+    private int currentIteration;
+    private final boolean[] currentSolution;
+    private boolean[] bestSolution;
+    private int bestFitness;
+
+    public RandomSearch(int problemSize, int maxIterations) {
+        if (problemSize < 1) {
+            throw new IllegalArgumentException("Problem size n must be above zero.");
+        }
+        this.problemSize = problemSize;
+
+        if (maxIterations < 1) {
+            throw new IllegalArgumentException("Maximum iterations must be above zero.");
+        }
+        this.maxIterations = maxIterations;
+
+        currentSolution = new boolean[problemSize];
+        bestSolution = new boolean[problemSize];
+    }
+
+    @Override
+    public String toString() {
+        return "RandomSearch{" + "currentIteration=" + currentIteration + "/" + maxIterations + ", bestFitness=" + bestFitness + "/" + problemSize + '}';
+    }
+
+    void run() {
+        for (int i = 0; i < maxIterations; i++) {
+            if (bestFitness == problemSize){
+                System.out.println("BEST SOLUTION FOUND!");
+                break;
+            }
+            currentIteration++;
+            randomizeCurrentSolution();
+            int sum = calcCurrentFitness();
+            if (sum > bestFitness) {
+                bestSolution = Arrays.copyOf(currentSolution, currentSolution.length);
+                bestFitness = sum;
+            }
+            System.out.println(this);
+        }
+        System.out.println("RandomSearch{" + "bestSolution=" + Arrays.toString(bestSolution));
+    }
+
+    private void randomizeCurrentSolution() {
+        for (int i = 0; i < currentSolution.length; i++) {
+            currentSolution[i] = new Random().nextBoolean();
+        }
+    }
+
+    private int calcCurrentFitness() {
+        int sum = 0;
+        for (int i = 0; i < currentSolution.length; i++)
+            if (currentSolution[i]) sum++;
+        return sum;
+    }
+}
