8add8ba2a51da1881bbc15f9f33503f9eef359c8
Sina <62741992+SinaAbdipoor@users.noreply.github.com>
2025-06-28 11:04:03 +0800

    Update Test.java

--- OneMax/MingXuan/Test.java
+++ OneMax/MingXuan/Test.java
@@ -2,8 +2,8 @@ package MingXuan;
 
 public class Test {
     public static void main(String[] args) {
-        int iter = 10000000;
-        int len = 1000000;
+        int iter = 10000;
+        int len = 10000;
 
 //        RandomSearch search = new RandomSearch(iter, len);
 //        search.iteration();
