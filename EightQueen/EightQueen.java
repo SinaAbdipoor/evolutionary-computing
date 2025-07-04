73d75417be9f090c287f4e0ecabeeeeb4e7c764f
Sina <62741992+SinaAbdipoor@users.noreply.github.com>
2025-07-04 18:30:53 +0800

    Update EightQueen.java

--- EightQueen/EightQueen.java
+++ EightQueen/EightQueen.java
@@ -1,41 +1,40 @@
 package EightQueen;
 
-public class EightQueen {
+import java.util.Arrays;
+
+class EightQueen {
     private final int[] board;
-    private int cost;
-    public EightQueen() {
+
+    EightQueen() {
         board = new int[8];
     }
 
-    public void placeQueen(int column, int row) {
+    int[] getBoard() {
+        return board;
+    }
+
+    @Override
+    public String toString() {
+        return "EightQueen{" +
+                "board=" + Arrays.toString(board) +
+                '}';
+    }
+
+    void printBoard() {
+
+    }
+
+    void placeQueen(int column, int row) {
         board[column] = row;
     }
 
-    public void calcCost() {
-        for (int col = 0;col < 8;col++) {
-            for (int j = 0;j < 8;j++) {
-                if (j != col)
-                    if (Math.abs(board[col] - board[j]) == Math.abs(col - j))
-                        cost++;
+    int calcCost() {
+        int cost = 0;
+        for (int col = 0; col < board.length; col++) {
+            for (int j = (col + 1); j < board.length; j++) {
+                if (Math.abs(board[col] - board[j]) == Math.abs(col - j)) cost++;
             }
         }
-    }
-
-    public int getCost() {
         return cost;
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        for (int col = 0;col < 8;col++) {
-            for (int row = 0;row < 8;row++) {
-                if (board[col] == row)
-                    sb.append(" Q ");
-                else
-                    sb.append(" . ");
-            }
-        }
-        return sb.toString();
     }
 }
