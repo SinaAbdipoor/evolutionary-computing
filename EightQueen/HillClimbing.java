6d6c7cad343c29d7a8bd99389065950aa844a816
syytt1 <143179298+syytt1@users.noreply.github.com>
2025-07-23 08:19:50 +0800

    HillClimbing Fix

--- EightQueen/HillClimbing.java
+++ EightQueen/HillClimbing.java
@@ -6,11 +6,62 @@ class HillClimbing {
     private int currentIteration = 0;
     private final int maxIterations;
     private EightQueen board;
+    private EightQueen newBoard;
+    private EightQueen currentBoard;
     private int cost;
+    private Random random = new Random();
 
     HillClimbing(int maxIterations) {
         if (maxIterations < 1) throw new IllegalArgumentException("Maximum iterations must be above zero.");
         this.maxIterations = maxIterations;
         // Start with a random board
+        board = new EightQueen();
+        randomizeBoard(board);
+        cost = board.calcCost();
+    }
+
+    @Override
+    public String toString() {
+        return "HillClimbing{" + "currentIteration=" + currentIteration + ", maxIterations=" + maxIterations + ", board=\n" + board + ", cost=" + cost + '}';
+    }
+
+    private void randomizeBoard(EightQueen Board) {
+        int size = board.getBoardLength();
+        for (int col = 0; col < size; col++) {
+            Board.placeQueen(col, random.nextInt(size));
+        }
+    }
+
+    private void climbTo(EightQueen Board) {
+        int size = Board.getBoardLength();
+        int newColumn = random.nextInt(size);
+        Board.placeQueen(newColumn, random.nextInt(size));
+    }
+
+    private void hillClimbing() {
+        currentIteration++;
+        currentBoard = newBoard;
+        climbTo(newBoard);
+        int newCost = newBoard.calcCost();
+        if (newCost < cost) {
+            cost = newCost;
+            board.copyFrom(newBoard);
+        }
+        else {
+            newBoard = currentBoard;
+        }
+    }
+
+
+    int run(boolean debugMode) {
+        newBoard = new EightQueen();
+        randomizeBoard(newBoard);
+        while (currentIteration < maxIterations) {
+            if (cost == 0) // Check if the best solution is found
+                return currentIteration;
+            hillClimbing(); // Run 1 iteration of random search
+            if (debugMode) System.out.println(this);
+        }
+        return -1; // Best solution not found
     }
 }
--- EightQueen/Test.java
+++ EightQueen/Test.java
@@ -14,5 +14,20 @@ public class Test {
         System.out.println("Random Search(5,000):" + new RandomSearch(5000).run(false));
         System.out.println("Random Search(10,000):" + new RandomSearch(10000).run(false));
         System.out.println("Random Search(100,000):" + new RandomSearch(100000).run(false));
+
+
+        System.out.println("Hill Climbing(5):" + new HillClimbing(5).run(false));
+        System.out.println("Hill Climbing(10):" + new HillClimbing(10).run(false));
+        System.out.println("Hill Climbing(20):" + new HillClimbing(20).run(false));
+        System.out.println("Hill Climbing(50):" + new HillClimbing(50).run(false));
+        System.out.println("Hill Climbing(50):" + new HillClimbing(50).run(false));
+        System.out.println("Hill Climbing(100):" + new HillClimbing(100).run(false));
+        System.out.println("Hill Climbing(200):" + new HillClimbing(200).run(false));
+        System.out.println("Hill Climbing(500):" + new HillClimbing(500).run(false));
+        System.out.println("Hill Climbing(1,000):" + new HillClimbing(1000).run(false));
+        System.out.println("Hill Climbing(2,000):" + new HillClimbing(2000).run(false));
+        System.out.println("Hill Climbing(5,000):" + new HillClimbing(5000).run(false));
+        System.out.println("Hill Climbing(10,000):" + new HillClimbing(10000).run(false));
+        System.out.println("Hill Climbing(100,000):" + new HillClimbing(100000).run(false));
     }
 }
